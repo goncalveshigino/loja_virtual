a8f068f439468da835007365e79f00ef865596ab
goncalveshigino <goncalveshigino20@gmail.com>
2021-01-19 23:15:21 +0100

    feita a tela principal

---
+++ lib/common/price_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/cart_manager.dart';
+import 'package:provider/provider.dart';
+
+class PriceCard extends StatelessWidget {
+
+
+
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  const PriceCard({this.buttonText, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final cartManager = context.watch<CartManager>();
+    final productsPrice = cartManager.productPrice;
+
+    return Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children:<Widget> [
+              Text(
+                "Resumo do pedido",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16
+                ),
+              ),
+
+              const SizedBox( height: 12,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Subtotal'),
+                  Text('R\$ ${productsPrice.toStringAsFixed(2)}'),
+                ],
+              ),
+
+              const Divider(),
+              const SizedBox(height: 12,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Total',
+                    style: TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    'R\$ ${productsPrice.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+
+              const SizedBox( height: 8,),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                color: Theme.of(context).primaryColor,
+                disabledColor: Theme.of(context).primaryColor.withAlpha(100),
+                textColor: Colors.white,
+                onPressed: onPressed,
+                child: Text(buttonText),
+              )
+            ],
+          ),
+        ),
+    );
+  }
+}
