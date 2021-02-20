ddc9a8d1b6112f9d4b53f23fc09dd8caf4ea54f8
goncalveshigino <goncalveshigono20@gmail.com>
2021-02-20 14:16:26 +0100

    validando

--- lib/Screen/edit_product/components/images_form.dart
+++ lib/Screen/edit_product/components/images_form.dart
@@ -17,6 +17,15 @@ class ImagesForm extends StatelessWidget {
 
     return FormField<List<dynamic>>(
       initialValue: List.from(product.images),
+      validator: (images){
+
+        if(images.isEmpty){
+            return 'Insira ao menos uma imagem';
+        }else {
+           return null;
+        }
+
+      },
       builder: (state){
         
          void onImageSeleted(File file){
@@ -25,67 +34,86 @@ class ImagesForm extends StatelessWidget {
            Navigator.of(context).pop();
        }
 
-        return AspectRatio(
-          aspectRatio: 1,
-          child: Carousel(
-            images: state.value.map<Widget>((image){
-              return Stack(
-                fit: StackFit.expand,
-                children: <Widget>[
+        return Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Carousel(
+                images: state.value.map<Widget>((image){
+                  return Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
 
-                    if(image is String)
-                        Image.network(image, fit: BoxFit.cover,)
-                   else
-                       Image.file(image as File, fit:  BoxFit.cover,),
+                        if(image is String)
+                            Image.network(image, fit: BoxFit.cover,)
+                       else
+                           Image.file(image as File, fit:  BoxFit.cover,),
 
-                  //Para alinhar em qualquer posicao
-                  Align(
-                    alignment: Alignment.topRight,
+                      //Para alinhar em qualquer posicao
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          icon: Icon(Icons.remove),
+                          color: Colors.red,
+                          onPressed: (){
+                             state.value.remove(image);
+                             state.didChange(state.value);
+                          },
+                        )
+                      )
+                    ],
+                  );
+                }).toList()..add(
+                  Material(
+                    color: Colors.grey[100],
                     child: IconButton(
-                      icon: Icon(Icons.remove),
-                      color: Colors.red,
+                      icon: Icon(Icons.add_a_photo),
+                      color: Theme.of(context).primaryColor,
+                      iconSize: 50,
                       onPressed: (){
-                         state.value.remove(image);
-                         state.didChange(state.value);
+                        
+                        if(Platform.isAndroid){
+                           showModalBottomSheet(
+                              context: context,
+                              builder: (_) => ImageSourceSheet(
+                                onImageSeleted: onImageSeleted
+                              )
+                          );
+                        }else {
+                          showCupertinoModalPopup(
+                              context: context,
+                              builder: (_) => ImageSourceSheet(
+                                onImageSeleted:onImageSeleted,
+                              )
+                          );
+                        }
                       },
-                    )
+                    ),
                   )
-                ],
-              );
-            }).toList()..add(
-              Material(
-                color: Colors.grey[100],
-                child: IconButton(
-                  icon: Icon(Icons.add_a_photo),
-                  color: Theme.of(context).primaryColor,
-                  iconSize: 50,
-                  onPressed: (){
-                    
-                    if(Platform.isAndroid){
-                       showModalBottomSheet(
-                          context: context,
-                          builder: (_) => ImageSourceSheet(
-                            onImageSeleted: onImageSeleted
-                          )
-                      );
-                    }else {
-                      showCupertinoModalPopup(
-                          context: context,
-                          builder: (_) => ImageSourceSheet(
-                            onImageSeleted:onImageSeleted,
-                          )
-                      );
-                    }
-                  },
                 ),
-              )
+                dotSize: 4,
+                dotSpacing: 15,
+                dotBgColor: Colors.transparent,
+                dotColor: Theme.of(context).primaryColor,
+                autoplay: false,
+              ),
             ),
-            dotSize: 4,
-            dotSpacing: 15,
-            dotBgColor: Colors.transparent,
-            dotColor: Theme.of(context).primaryColor,
-            autoplay: false,
-          ),
+
+      //Verificar se existe algum erro
+            if(state.hasError)
+               Container(
+                 alignment: Alignment.centerLeft,
+                 margin: const EdgeInsets.only(top: 16, left: 16),
+                 child: Text(
+                   state.errorText,
+                   style: const TextStyle(
+                     color: Colors.red,
+                     fontSize: 12,
+                     fontWeight: FontWeight.bold
+                   ),
+                 ),
+               )
+          ],
         );
       },
     );
