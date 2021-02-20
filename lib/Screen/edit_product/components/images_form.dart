1a552751a767aeccd5bf9e1269ef6ef89b11c2aa
goncalveshigino <goncalveshigono20@gmail.com>
2021-02-20 13:16:22 +0100

    Escolher imagem da galeria ou tirar da camera

--- lib/Screen/edit_product/components/images_form.dart
+++ lib/Screen/edit_product/components/images_form.dart
@@ -14,9 +14,17 @@ class ImagesForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return FormField<List<dynamic>>(
-      initialValue: product.images,
+      initialValue: List.from(product.images),
       builder: (state){
+        
+         void onImageSeleted(File file){
+           state.value.add(file);
+           state.didChange(state.value);
+           Navigator.of(context).pop();
+       }
+
         return AspectRatio(
           aspectRatio: 1,
           child: Carousel(
@@ -56,12 +64,16 @@ class ImagesForm extends StatelessWidget {
                     if(Platform.isAndroid){
                        showModalBottomSheet(
                           context: context,
-                          builder: (_) => ImageSourceSheet()
+                          builder: (_) => ImageSourceSheet(
+                            onImageSeleted: onImageSeleted
+                          )
                       );
                     }else {
                       showCupertinoModalPopup(
                           context: context,
-                          builder: (_) => ImageSourceSheet()
+                          builder: (_) => ImageSourceSheet(
+                            onImageSeleted:onImageSeleted,
+                          )
                       );
                     }
                   },
