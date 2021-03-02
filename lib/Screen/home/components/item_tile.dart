6bc6cbd19cf9d8f6f79e9fb7e3c061f75794895f
goncalveshigino <goncalveshigono20@gmail.com>
2021-03-02 23:54:21 +0100

    remover

--- lib/Screen/home/components/item_tile.dart
+++ lib/Screen/home/components/item_tile.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/home_manager.dart';
 import 'package:loja_virtual/models/product_manager.dart';
 import 'package:loja_virtual/models/section_item.dart';
 import 'package:provider/provider.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:provider/provider.dart';
 
 class ItemTile extends StatelessWidget {
 
@@ -14,7 +16,11 @@ class ItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+  final homeManager = context.watch<HomeManager>();
+
     return GestureDetector(
+
        onTap: (){
          if(item.product != null){
 
@@ -26,6 +32,25 @@ class ItemTile extends StatelessWidget {
            }
          }
        },
+       onLongPress: homeManager.editing ?(){
+           showDialog(
+             context: context,
+             builder: (_){
+               return AlertDialog(
+                 title:  const Text('Editar Texto'),
+                 actions: <Widget>[
+                   FlatButton(
+                     onPressed: (){
+
+                     },
+                     textColor: Colors.red,
+                     child:  const Text('Excluir'),
+                   )
+                 ],
+               );
+             }
+           );
+       }: null,
        child: AspectRatio(
          aspectRatio: 1,
          child: item.image is String 
