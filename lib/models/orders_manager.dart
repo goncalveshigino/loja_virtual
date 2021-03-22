add74858de72c067ae722aaad05527550595ece8
adilsonchameia <adilsonkchameia@outlook.com>
2021-03-22 21:20:31 +0100

    Buscando pedidos no firebase

---
+++ lib/models/orders_manager.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:loja_virtual/models/user.dart';
+
+import 'order.dart';
+
+class OrdersManager extends ChangeNotifier {
+  User user;
+
+  List<Order> orders = [];
+
+  final Firestore firestore = Firestore.instance;
+
+  void updateUser(User user) {
+    this.user = user;
+
+    if (user != null) {
+      _listenToOrders();
+    }
+  }
+
+  void _listenToOrders() {
+
+    firestore
+        .collection('orders')
+        .where('user', isEqualTo: user.id)
+        .snapshots()
+        .listen((event) {
+          
+      orders.clear();
+      for (final doc in event.documents) {
+        orders.add(Order.fromDocument(doc));
+      }
+
+      print(orders);
+
+    });
+  }
+}
