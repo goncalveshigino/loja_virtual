21ee8a7aa021a2567932f667a3773fa7a7dea2c0
adilsonchameia <adilsonkchameia@outlook.com>
2021-04-10 23:01:20 +0100

    Exibindo as Lojas

---
+++ lib/Screen/stores/components/store_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/common/custom_icon_button.dart';
+import 'package:loja_virtual/models/stores/store.dart';
+
+class StoreCard extends StatelessWidget {
+
+
+  const StoreCard(this.store);
+
+  final Store store;
+  
+  @override
+  Widget build(BuildContext context) {
+
+    final primaryColor = Theme.of(context).primaryColor;
+
+    return Card(
+       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+       child: Column(
+         children: [
+           Image.network(store.image),
+           Container(
+             height: 140,
+             padding: const EdgeInsets.all(16),
+             child: Row(
+               children: [
+                 Expanded(
+                   child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: [
+                      Text(
+                        store.name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 17
+                        ),
+                      ),
+                      Text(
+                        store.addressText,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                     ],
+                   ),
+                 ),
+                 Column(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+                      CustomIconButton(
+                        iconData: Icons.map,
+                        color: primaryColor,
+                        onTap: (){
+
+                        },
+                      ),
+                       CustomIconButton(
+                        iconData: Icons.phone,
+                        color: primaryColor,
+                        onTap: (){
+
+                        },
+                      )
+                   ],
+                 )
+               ],
+             ),
+           )
+         ],
+       ),
+    );
+  }
+}
