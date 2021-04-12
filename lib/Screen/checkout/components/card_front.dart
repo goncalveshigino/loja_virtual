d2c25fc30f586d69bfe965b93e583799553ad3d5
adilsonchameia <adilsonkchameia@outlook.com>
2021-04-12 01:43:34 +0100

    Criando a parte frontal do cartao

--- lib/Screen/checkout/components/card_front.dart
+++ lib/Screen/checkout/components/card_front.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'card_text_field.dart';
+
 class CardFront extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,6 +12,47 @@ class CardFront extends StatelessWidget {
       child: Container(
         height: 200,
         color: const Color(0xFF1B4B52),
+        padding: EdgeInsets.all(24),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                 children: [
+                    CardTextField(
+                      title: 'Número',
+                      hint: '0000 0000 0000 0000',
+                      textInputType: TextInputType.number,
+                      bold: true,
+                    ),
+                     CardTextField(
+                      title: 'Validade',
+                      hint: '04/2020',
+                      textInputType: TextInputType.number,
+                    ),
+                     CardTextField(
+                      title: 'Título',
+                      hint: 'Gonçalves Luis Higino',
+                      textInputType: TextInputType.text,
+                      bold: true,
+                    )
+                 ],
+              ),
+            ),
+            Stack(
+              children: [
+                Icon(
+                  Icons.credit_card,
+                  color: Colors.white,
+                  size: 44,
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
